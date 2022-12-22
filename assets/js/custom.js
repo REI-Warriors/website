e48a72afa459e1665afd7f94d65c894f8123844e
Brendan Roberts <brendan.m.roberts@gmail.com>
2022-12-22 14:43:35 -0700

    top nav on bootstrap 5 😒

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,71 +1,35 @@
 $(document).ready(function(){
 	"use strict";
     
-        /*==================================
-* Author        : "ThemeSine"
-* Template Name : Khanas HTML Template
-* Version       : 1.0
-==================================== */
+	/*
+	* Many thanks to the original template author: "ThemeSine"
+	* Template Name : Khanas HTML Template
+	*/
 
-
-
-/*=========== TABLE OF CONTENTS ===========
-1. Scroll To Top 
-2. Smooth Scroll spy
-3. Progress-bar
-4. owl carousel
-5. welcome animation support
-======================================*/
-
-    // 1. Scroll To Top 
-		$(window).on('scroll',function () {
-			if ($(this).scrollTop() > 600) {
-				$('.return-to-top').fadeIn();
-			} else {
-				$('.return-to-top').fadeOut();
-			}
-		});
-		$('.return-to-top').on('click',function(){
-				$('html, body').animate({
-				scrollTop: 0
-			}, 1500);
-			return false;
-		});
+    // Scroll To Top 
+	$(window).on('scroll',function () {
+		if ($(this).scrollTop() > 600) {
+			$('.return-to-top').fadeIn();
+		} else {
+			$('.return-to-top').fadeOut();
+		}
+	});
+	$('.return-to-top').on('click',function(){
+			$('html, body').animate({
+			scrollTop: 0
+		}, 1500);
+		return false;
+	});
 	
-	
-	
-	// 2. Smooth Scroll spy
-		
-		$('.header-area').sticky({
-           topSpacing:0
-        });
-		
-		//=============
+	// welcome animation support
+	$(window).load(function(){
+		$(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
+		$(".header-text a").removeClass("animated fadeInDown").css({'opacity':'0'});
+	});
 
-		$('li.smooth-menu a').bind("click", function(event) {
-			event.preventDefault();
-			var anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $(anchor.attr('href')).offset().top - 0
-			}, 1200,'easeInOutExpo');
-		});
-		
-		$('body').scrollspy({
-			target:'.navbar-collapse',
-			offset:0
-		});
-
-	// 5. welcome animation support
-
-        $(window).load(function(){
-        	$(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
-            $(".header-text a").removeClass("animated fadeInDown").css({'opacity':'0'});
-        });
-
-        $(window).load(function(){
-        	$(".header-text h2,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
-            $(".header-text a").addClass("animated fadeInDown").css({'opacity':'0'});
-        });
-
+	$(window).load(function(){
+		$(".header-text h2,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
+		$(".header-text a").addClass("animated fadeInDown").css({'opacity':'0'});
+	});
 });	
 	
